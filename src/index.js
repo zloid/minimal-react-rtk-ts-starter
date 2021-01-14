46a06d5eb44ef5f8ca9a03cf6dbae6106c06be9c
zloid <zloid-1@yandex.by>
2021-01-14 21:07:03 +0100

    add: basic React to JS converter

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const SomeReactComponent = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <h2>World</h2>
+            <h3><u>and React!</u></h3>
+        </div>
+    )
+}
+
+render(<SomeReactComponent />, document.getElementById('root'))
